58c957f364602bcd4edd2e017ed0bb554056617b
Araknoss <alvarocolladogarcia1@gmail.com>
2025-11-12 12:52:20 +0100

    RendererController
    FadeOut&ResetMaterial

--- Assets/Scripts/DefeatedState.cs
+++ Assets/Scripts/DefeatedState.cs
@@ -1,54 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class DefeatedState : StateMachineBehaviour
 {
-    private EnemyAIContext _context;
+    private EnemyAIContext _context;    
+    private RendererController _rendererController;
     private Rigidbody body;
-    private Material material;
-    private Color initialColor;
-    private float defeatTimer;
+    private float defeatedTimer;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _context = animator.GetComponent<EnemyAIContext>();
 
+        _rendererController = animator.GetComponent<RendererController>();
+
         body = animator.GetComponent<Rigidbody>();
         body.isKinematic = true;
 
-        material = animator.gameObject.GetComponent<Renderer>().material;
-        initialColor = material.color;
-    }
+        _rendererController.FadeOutMaterialColor(_context.defeatDuration);
+    }    
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(defeatTimer < _context.defeatDuration)
+        defeatedTimer+=Time.deltaTime;
+        if(defeatedTimer >= _context.defeatDuration)
         {
-            defeatTimer += Time.deltaTime;
-            float alpha = Mathf.Lerp(1f, 0f, defeatTimer / _context.defeatDuration);
-            material.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
-
-            Debug.Log("Alpha: " + alpha);   
+            _context.gameObject.SetActive(false);
+            defeatedTimer = 0f;
         }
-        else
-        {
-            animator.gameObject.SetActive(false);
-        }
-
     }
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        material.color = initialColor;
-
+    {     
+        _rendererController.ResetMaterialColor();
         body.isKinematic = false;
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+      
 }
---
+++ Assets/Scripts/RendererController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class RendererController : MonoBehaviour
+{  
+    private Material material;
+    private Color initialColor;
+    private void Awake()
+    {
+        material = gameObject.GetComponent<Renderer>().material;
+        initialColor = material.color;
+    }
+    public void ResetMaterialColor()
+    {
+        material.color = initialColor;
+    }
+    public void FadeOutMaterialColor(float duration)
+    {
+        StartCoroutine(FadeOutCo(duration));        
+    }
+
+    IEnumerator FadeOutCo(float duration)
+    {        
+        float timeElapsed = 0f;
+        while (timeElapsed < duration)
+        {
+            timeElapsed+= Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, timeElapsed / duration);
+            material.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);    
+            yield return null;
+
+            //Debug.Log("Fading... Alpha: " + alpha);
+        }
+        material.color = new Color(initialColor.r, initialColor.g, initialColor.b, 0f);
+    }
+}
